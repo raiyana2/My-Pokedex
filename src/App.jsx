b8907c63458f725c8a8691992f9ef74d16ac0db9
Raiyana Rahman <raiyana2@ualberta.ca>
2025-10-04 00:08:57 -0600

    added my incomplete pokedex website

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Header } from "./components/Header"
+import { SideNav } from "./components/SideNav"
+import { PokeCard } from "./components/PokeCard"
+import viteLogo from '/vite.svg'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <SideNav /> 
+
+      <PokeCard />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/SideNav.jsx
@@ -0,0 +1,16 @@
+import { first151Pokemon } from "../utils"
+
+
+export function SideNav() {
+  return (
+    <nav>
+        {first151Pokemon.map((pokemon, pokemonIndex ) => {
+            return(
+                <button>
+                   <p>{pokemon}</p>
+                </button>
+            )
+    })}
+    </nav>
+  )
+}
