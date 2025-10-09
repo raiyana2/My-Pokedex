380aa7f726a5f4991257d0991f47d3bc02afe2d2
Raiyana Rahman <raiyana2@ualberta.ca>
2025-10-08 23:17:35 -0600

    haha

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,20 @@ import { Header } from "./components/Header"
 import { SideNav } from "./components/SideNav"
 import { PokeCard } from "./components/PokeCard"
 import viteLogo from '/vite.svg'
+import { useState } from 'react'
 
 function App() {
+  const [selectedPokemon, setSelectedPokemon] = useState(0);
 
   return (
     <>
       <Header />
-      <SideNav /> 
+      <SideNav selectedPokemon={selectedPokemon} setSelectedPokemon=
+      {selectedPokemon} /> 
 
-      <PokeCard />
+      <PokeCard selectedPokemon={selectedPokemon} />
     </>
-  )
+  ) 
 }
 
 export default App
